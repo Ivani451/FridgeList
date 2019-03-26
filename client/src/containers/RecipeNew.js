7e8d75556f5d483d0f42cfb90fe5e2005a738005
Ivani Aviles <ivaniaviles@gmail.com>
2019-03-25 22:49:11 -0500

    added form elements to RecipeNew

--- client/src/containers/RecipeNew.js
+++ client/src/containers/RecipeNew.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
 
-export default class RecipeNew extends Component {
+class RecipeNew extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const data = this.state;
+    console.log(data);
+  };
+
+  handleInputChange = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
+    const { title } = this.state;
     return (
       <div>
-        <h1>Hello</h1>
+        <p>title is {title}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input
+              type="text"
+              placeholder="Title"
+              name="title"
+              onChange={this.handleInputChange}
+            />
+          </p>
+          <p>
+            <button>Send</button>
+          </p>
+        </form>
       </div>
     );
   }
 }
+
+export default RecipeNew;
