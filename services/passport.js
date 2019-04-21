5ffc3180bc550f38996da1a204d8b6511d441650
Ivani Aviles <ivaniaviles@gmail.com>
2019-04-20 22:27:28 -0500

    setup passport google strategy for user creation

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
+const pg = require("pg");
+require("dotenv").config();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+
+// database configuration
+let pool = new pg.Pool({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  max: process.env.DB_MAX
+});
 
 // passport setup using a Google strategy.
 // callbackURL is the URL that passport sends the user to after permission is granted to Google
@@ -9,12 +21,30 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
+      pool.connect((poolErr, poolClient, poolDone) => {
+        if (poolErr) {
+          return console.error("pool client fetch error", poolErr);
+        }
+        poolClient.query(
+          "SELECT id FROM users WHERE id=$1",
+          [profile.id],
+          (error, result) => {
+            if (error) {
+              return console.error("query error", error);
+            } else {
+              return console.log("success!");
+            }
+          }
+        );
+      });
+
       console.log("accessToken: ", accessToken);
       console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
+      console.log("googleID: ", profile.id);
     }
   )
 );
