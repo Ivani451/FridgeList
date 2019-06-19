f80654f34f747f3950677ce092f512f6e09e3ef6
Ivani Aviles <ivaniaviles@gmail.com>
2019-06-18 20:55:13 -0500

    fixed format of saved personal recipe

--- client/src/containers/MyFoodInfo.js
+++ client/src/containers/MyFoodInfo.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Recipe from "./Recipe";
 
+// the recipe is passed down to the props from our database as an object
 class MyFoodInfo extends Component {
   render() {
+    let recipe = this.props.recipes;
+
     return (
-      <div>
-        <h1>{this.props.recipes.title}</h1>
+      <div id="my-food-info">
+        <h1 id="food-title">{recipe.title}</h1>
+
+        <p>
+          <span>Prep-time: </span>
+          {recipe.prep}
+        </p>
+
+        <p>
+          <span>Servings: </span>
+          {recipe.servings}
+        </p>
+
+        <div>
+          <span>Ingredients: </span>
+          {recipe.ingredients}
+        </div>
+
+        <p>
+          <span>Instructions: </span>
+          {recipe.instructions}
+        </p>
+
+        <p>
+          By <a>{recipe.author}</a>
+        </p>
       </div>
     );
   }
