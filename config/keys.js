d35285f6a7dfde1b24636379da8f06a6c186c741
Ivani Aviles <ivaniaviles@gmail.com>
2019-06-23 19:41:30 -0500

    production/development enviroment setup

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === "production") {
+  // return the production set of keys
+  module.exports = require("./prod");
+} else {
+  // return the development set of keys
+  module.exports = require("./dev");
+}
