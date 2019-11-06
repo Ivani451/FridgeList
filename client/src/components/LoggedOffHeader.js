7e86781f0a0fa7939a14a39e3ae0f0e2e162e855
Ivani Aviles <ivaniaviles@gmail.com>
2019-11-05 22:17:41 -0600

    modified headers to be more dynamic with screen size

--- client/src/components/LoggedOffHeader.js
+++ client/src/components/LoggedOffHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import SearchBar from "containers/SearchBar";
 
 //  Hyperlink tags used for refreshing the page in order to be directed into the oauth flow when clicking on the login button
 const LoggedOffHeader = () => {
@@ -9,6 +10,8 @@ const LoggedOffHeader = () => {
         <a href="/Landing" id="header-logo">
           <h2>Fridge List</h2>
         </a>
+
+        <SearchBar />
 
         <a href="/auth/google/" className="header-button" id="google-login">
           <p>Login with Google</p>
@@ -26,31 +29,17 @@ const LoggedOffHeader = () => {
           <h3>Fridge List</h3>
         </a>
 
-        <a href="/recipes" className="header-button">
-          <p>Home</p>
-        </a>
-
-        <NavLink
-          to="/recipe/new"
-          activeClassName="active"
-          className="header-button"
-        >
-          <p>Add Recipe</p>
-        </NavLink>
-
-        <a href="/recipe/search" className="header-button">
-          <p>Search Recipes</p>
-        </a>
-
-        <a href="/api/logout/" className="header-button" id="google-login">
-          <p>Logout</p>
+        <a href="/auth/google/" className="header-button" id="google-login">
+          <p>Login with Google</p>
         </a>
       </nav>
     </div>
   );
 };
 
-// main-header-medium is the header that is showed to the user when the screen size
-// is either 800px or below.
+/* 
+  main-header-medium is the header that is showed to the user when the screen size
+  is either 800px or below.
+*/
 
 export default LoggedOffHeader;
--- client/src/components/LoggedOnHeader.js
+++ client/src/components/LoggedOnHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import SearchBar from "containers/SearchBar";
 
 /* 
   NavLink used for quicker navigation to the "new recipe" page. Otherwise, a regular "href" element is used to refresh page completely
@@ -9,25 +10,46 @@ const LoggedOnHeader = () => {
   return (
     <div>
       <nav class="main-header-large">
+        <a
+          href="JavaScript:void(0)"
+          id="hamburger_icon"
+          onClick={() => {
+            var x = document.getElementById("nav-links");
+            if (x.style.display === "block") {
+              x.style.display = "none";
+            } else {
+              x.style.display = "block";
+            }
+          }}
+        >
+          <div class="hamburger_line"></div>
+          <div class="hamburger_line"></div>
+          <div class="hamburger_line"></div>
+        </a>
+
         <a href="/recipes" id="header-logo">
           <h3>Fridge List</h3>
         </a>
 
-        <a href="/" className="header-button">
-          <p>Home</p>
-        </a>
+        <div id="nav-links">
+          <a href="/" className="header-button">
+            <p>Home</p>
+          </a>
 
-        <a href="/recipes" className="header-button">
-          <p>My Recipes</p>
-        </a>
+          <a href="/recipes" className="header-button">
+            <p>My Recipes</p>
+          </a>
 
-        <NavLink
-          to="/recipe/new"
-          activeClassName="active"
-          className="header-button"
-        >
-          <p>Add Recipe</p>
-        </NavLink>
+          <NavLink
+            to="/recipe/new"
+            activeClassName="active"
+            className="header-button"
+          >
+            <p>Add Recipe</p>
+          </NavLink>
+        </div>
+
+        <SearchBar />
 
         <a href="/api/logout/" className="header-button" id="google-login">
           <p>Logout</p>
@@ -72,6 +94,8 @@ const LoggedOnHeader = () => {
         <a href="/recipes" id="header-logo">
           <h3>Fridge List</h3>
         </a>
+
+        <SearchBar />
       </nav>
     </div>
   );
