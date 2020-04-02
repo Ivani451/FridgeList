745bdc8be3828349442d6661f07d0ad995e4b0d0
Ivani Aviles <ivaniaviles@gmail.com>
2020-04-01 21:40:26 -0500

    altered production key configuration

--- config/prod.js
+++ config/prod.js
@@ -2,10 +2,10 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
-  DB_USER: "aws_postgres_prod",
-  DB_DATABASE: "aws_postgres_prod",
-  DB_PASSWORD: "haze2tha451",
-  DB_HOST: "fridgelist-prod.cwjnu9ovvpww.us-east-2.rds.amazonaws.com",
+  DB_USER: process.env.DB_USER,
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
   DB_PORT: "5432",
   DB_MAX: "10"
 };
