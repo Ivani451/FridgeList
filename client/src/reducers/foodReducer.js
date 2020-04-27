7cb8d903ec1ed0afbf84e4a4c8b7fac809fa3b90
Ivani Aviles <ivaniaviles@gmail.com>
2020-04-26 23:52:44 -0500

    changed database configuration to allow for production posting of recipes

--- client/src/reducers/foodReducer.js
+++ client/src/reducers/foodReducer.js
@@ -3,9 +3,9 @@ import { FETCH_FOOD, FETCH_RECIPES } from "../actions/types";
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_FOOD:
-      console.log(action.payload);
       return [action.payload.data];
     case FETCH_RECIPES:
+      console.log(action.payload);
       return action.payload;
     default:
       return state;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const pg = require("pg");
 require("dotenv").config();
+const keys = require("../config/keys");
 
 // database configuration
 let pool = new pg.Pool({
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  max: process.env.DB_MAX
+  user: keys.DB_USER,
+  database: keys.DB_DATABASE,
+  password: keys.DB_PASSWORD,
+  host: keys.DB_HOST,
+  port: keys.DB_PORT,
+  max: keys.DB_MAX
 });
 
 module.exports = app => {
