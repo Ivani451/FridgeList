8c03232559d5ae9d03d7af49a3c9f7ed86c9157e
Ivani Aviles <ivaniaviles@gmail.com>
2020-07-01 23:08:52 -0500

    changed config to hopefully allow production access to our database

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,12 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  cookieKey: process.env.COOKIE_KEY,
-  DB_USER: process.env.DB_USER,
-  DB_DATABASE: process.env.DB_DATABASE,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_MAX: process.env.DB_MAX
+  googleClientID:
+    "777423450339-4bc6nttl1a08baadnimnhj3n6gsrcu4t.apps.googleusercontent.com",
+  googleClientSecret: "dwwvsXwmqLllnGadSu4vMSkd",
+  cookieKey: "JDSFGKLJ4EK63KL6J346LJ27JK23L4",
+  DB_USER: "aws_postgres_prod",
+  DB_DATABASE: "aws_postgres_prod",
+  DB_PASSWORD: "haze2tha451",
+  DB_HOST: "fridgelist-prod.cwjnu9ovvpww.us-east-2.rds.amazonaws.com",
+  DB_PORT: "5432",
+  DB_MAX: "10",
 };
